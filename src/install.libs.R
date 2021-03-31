0bdb38bd837bf4e3d4d8311c297cac355ecc5519
Michael Rustler <michael.rustler@kompetenz-wasser.de>
2021-03-31 09:50:37 +0200

    add v5.0.13 source code from https://www.epa.gov/sites/production/files/2018-08/swmm51013_engine_0.zip

---
+++ src/install.libs.R
@@ -0,0 +1,13 @@
+file <- if (WINDOWS) {
+  "swmmm5.exe"}  else {
+    "swmm5"}
+
+# working directory is ./src
+if (file.exists(file)) {
+  dest <- file.path(R_PACKAGE_DIR, "bin", R_ARCH)
+  message("Installing ", file, " to ", dest)
+  dir.create(dest, recursive=TRUE, showWarnings=FALSE)
+  file.copy(file, dest, overwrite=TRUE)
+  file.remove(c(file, list.files(pattern = "\\.o$")))
+  
+}
