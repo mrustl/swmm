f83a0df5c0fa9bc26d3d8ee8794b6ebe9bd3ae60
mrustl <michael.rustler@kompetenz-wasser.de>
2021-07-08 16:19:11 +0200

    New makefile

--- src/install.libs.R
+++ src/install.libs.R
@@ -9,5 +9,19 @@ if (file.exists(file)) {
   dir.create(dest, recursive=TRUE, showWarnings=FALSE)
   file.copy(file, dest, overwrite=TRUE)
   file.remove(c(file, list.files(pattern = "\\.o$")))
-  
+
+}
+
+file <- if (WINDOWS) {
+  "swmmm5.dll"}  else {
+    "swmm5.dll"}
+
+# working directory is ./src
+if (file.exists(file)) {
+  dest <- file.path(R_PACKAGE_DIR, "bin", R_ARCH)
+  message("Installing ", file, " to ", dest)
+  dir.create(dest, recursive=TRUE, showWarnings=FALSE)
+  file.copy(file, dest, overwrite=TRUE)
+  file.remove(c(file, list.files(pattern = "\\.o$")))
+
 }
